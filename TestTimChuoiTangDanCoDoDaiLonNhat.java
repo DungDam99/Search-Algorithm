c007f13e8b6fbae2e3f11bd738f21f6dfb1f1ee7
DungDam99 <40400279+DungDam99@users.noreply.github.com>
2018-11-19 01:51:48 +0700

    Update TestTimChuoiTangDanCoDoDaiLonNhat.java

--- TestTimChuoiTangDanCoDoDaiLonNhat.java
+++ TestTimChuoiTangDanCoDoDaiLonNhat.java
@@ -2,7 +2,7 @@ package SearchAlgorithm.TimChuoiTangDanCoDoDaiLonNhat;
 
 public class TestTimChuoiTangDanCoDoDaiLonNhat {
     public static void main(String[] args) {
-        String s = "Welcome";
+        String s = "1284691";
         String result = TimChuoiTangDanCoDoDaiLonNhat.timChuoiTangDanCoDoDaiLonNhat(s);
         System.out.println(result);
     }
